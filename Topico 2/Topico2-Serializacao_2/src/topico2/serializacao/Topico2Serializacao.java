1b3ba162fa5497979d505b49cd4ced5d6379e9c9
GuilhermeARS <guiars@hotmail.com>
2018-04-28 00:44:37 -0300

    tarefa2-2

---
+++ Topico 2/Topico2-Serializacao_2/src/topico2/serializacao/Topico2Serializacao.java
@@ -0,0 +1,55 @@
+// Aluno: Guilherme Alves Rosa e Silva
+
+package topico2.serializacao;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Topico2Serializacao {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ClasseTeste paraSerializar = new ClasseTeste(); 
+        ClasseTeste Serializado;
+        byte[] array = Serializa(paraSerializar); 
+        System.out.println(Arrays.toString(array));
+        Serializado = Deserializa(array);
+        System.out.println(Serializado.getTexto());
+            
+        
+
+    }
+// Serializa um objeto do tipo ClasseTeste
+    public static byte[] Serializa(ClasseTeste pSer) throws IOException{
+        ByteArrayOutputStream baoos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(baoos)) {
+            oos.writeObject(pSer);
+            oos.close();
+        }
+        
+        return baoos.toByteArray();
+    }
+             
+    // Deserializa um array de bytes retornando um objeto do tipo ClasseTeste
+    public static ClasseTeste Deserializa(byte[] array) throws IOException, ClassNotFoundException{
+        ByteArrayInputStream bais = new ByteArrayInputStream(array);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return (ClasseTeste) ois.readObject();
+    }
+    
+    //Classe a ser serializada
+    public static class ClasseTeste implements Serializable {
+
+        private String texto = "deu certo";
+
+        public String getTexto() {
+            return texto;
+        }
+        
+    }
+
+}
